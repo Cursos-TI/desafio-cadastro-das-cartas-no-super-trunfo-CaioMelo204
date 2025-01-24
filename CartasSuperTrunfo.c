16c72c404dd5819a8550c217905e0079866bfb07
CaioMelo204 <167645355+CaioMelo204@users.noreply.github.com>
2025-01-23 22:49:05 -0300

    second badge

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Card {
     char city[25];
@@ -6,6 +7,8 @@ struct Card {
     float area;
     float PIB;
     int tourist;
+    float densityPopulation;
+    float PIBPerPerson;
 };
 
 struct State {
@@ -17,6 +20,37 @@ struct Country {
     char name[20];
     struct State state[8];
 };
+
+struct Card* createCard() {
+    struct Card *card = (struct Card *)malloc(sizeof(struct Card));
+    printf("Enter city: ");
+    scanf("%s", &card->city);
+    printf("Enter population: ");
+    scanf("%d", &card->population);
+    printf("Enter area: ");
+    scanf("%f", &card->area);
+    printf("Enter PIB: ");
+    scanf("%f", &card->PIB);
+    printf("Enter tourist: ");
+    scanf("%d", &card->tourist);
+    printf("\n");
+    card->densityPopulation = (float) card->population / card->area;
+    card->PIBPerPerson = (float) card->PIB / card->population;
+
+    return card;
+}
+
+void displayCard(struct Card *card) {
+    printf("******************\n");
+    printf("%s\n", card->city);
+    printf("Population: %d\n", card->population);
+    printf("Area: %.2f\n", card->area);
+    printf("PIB: %.2f\n", card->PIB);
+    printf("Tourist: %d\n", card->tourist);
+    printf("DensityPopulation: %.2f\n", card->densityPopulation);
+    printf("PIBPerPerson: %.2f\n", card->PIBPerPerson);
+    printf("******************\n");
+}
 
 int main(void)
 {
@@ -30,28 +64,18 @@ int main(void)
         scanf("%s", country.state[i].name);
 
         for (int j = 0; j < 4; j++) {
-            printf("Enter city: ");
-            scanf("%s", country.state[i].card[j].city);
-            printf("Enter population: ");
-            scanf("%d", &country.state[i].card[j].population);
-            printf("Enter area: ");
-            scanf("%f", &country.state[i].card[j].area);
-            printf("Enter PIB: ");
-            scanf("%f", &country.state[i].card[j].PIB);
-            printf("Enter tourist: ");
-            scanf("%d", &country.state[i].card[j].tourist);
+            country.state[i].card[i] = *createCard();
+            break;
         }
+        break;
     }
 
     for (int i = 0; i < 8; i++) {
+        printf("********* STATE ************ \n");
         printf("%s\n", country.state[i].name);
         for (int j = 0; j < 4; j++) {
-            printf("%s ", country.state[i].name);
-            printf("%s ", country.state[i].card[j].city);
-            printf("%d ", country.state[i].card[j].population);
-            printf("%f ", country.state[i].card[j].area);
-            printf("%f ", country.state[i].card[j].PIB);
-            printf("%d ", country.state[i].card[j].tourist);
+            displayCard(&country.state[i].card[j]);
         }
+        printf("***************************** \n");
     }
 }
